f9b7c6e0670f3a28b3bbfafedf0143833f775267
Joseph Bergantine <jbergantine@gmail.com>
2012-12-18 13:44:57 -0700

    A

--- django_contact_form/forms.py
+++ django_contact_form/forms.py
@@ -5,6 +5,13 @@ class ContactForm(forms.Form):
     subject = forms.CharField(max_length=100)
     email = forms.EmailField(label='Your email address')
     message = forms.CharField(widget=forms.Textarea)
+    captcha = forms.CharField(label='What is the sum of six and seven?')
+
+    def clean_captcha(self):
+        captcha = self.cleaned_data['captcha']
+        if not re.search(r"thirteen|13", capcha, re.IGNORECASE):
+            raise forms.ValidationError("Double check your math.")
+        return captcha
 
     def clean_message(self):
         """
