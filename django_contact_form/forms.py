2e1af8a3537deaae7b4bd3863aa571b4a8b57118
Joseph Bergantine <jbergantine@gmail.com>
2012-12-18 13:49:04 -0700

    Removed excessive comments of clean_message().

--- django_contact_form/forms.py
+++ django_contact_form/forms.py
@@ -14,23 +14,9 @@ class ContactForm(forms.Form):
         return captcha
 
     def clean_message(self):
-        """
-        Simple custom validation example. Require 4+ words in the message field.
-        
-        Django's forms system automatically looks for any method whose name 
-        starts with clean_ and ends with the name of the field. If any such 
-        methods exist, they're called during validation.
-        
-        Specifically, clean_message() will be called after the default 
-        validation logic for a given field.
-        """
         message = self.cleaned_data['message']
         num_words = len(message.split())
         if num_words < 4:
             raise forms.ValidationError("This must be at least 4 words in"\
                                         " length.")
-        return message # Explicitly return the cleaned value at the end of the 
-                       # message to allow us to modify the value within our 
-                       # custom validation method. If we don't return it 
-                       # explicitly, None will be returned and the original 
-                       # value will be lost.
+        return message
