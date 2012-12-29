375b9817376c5d1c2202b7403d2297c150673e37
Joseph Bergantine <jbergantine@gmail.com>
2012-12-29 13:37:44 -0700

    HTML5 forms.

--- django_contact_form/forms.py
+++ django_contact_form/forms.py
@@ -1,12 +1,14 @@
 import re
 
-from django import forms
+import floppyforms as forms
 
 
 class ContactForm(forms.Form):
-    email = forms.EmailField(label='Your email address')
+    email = forms.EmailField(label='Your email address',
+        widget=forms.EmailInput())
     message = forms.CharField(widget=forms.Textarea)
-    captcha = forms.CharField(label='What is the sum of six and seven?')
+    captcha = forms.CharField(label='What is the sum of six and seven?',
+        widget=forms.NumberInput())
 
     def clean_captcha(self):
         captcha = self.cleaned_data['captcha']
