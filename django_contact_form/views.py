848729c89361bab2edcb8073d1fd8cead218acc5
Joseph Bergantine <jbergantine@gmail.com>
2013-07-11 10:50:01 -0600

    Removed unused import.

--- django_contact_form/views.py
+++ django_contact_form/views.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.contrib.sites.models import Site
 from django.core.mail import send_mail
 from django.core.urlresolvers import reverse
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django_contact_form.forms import ContactForm
