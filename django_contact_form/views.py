d48caa4d4d81bdb86edccb44168e356fd3cf661c
Joseph Bergantine <jbergantine@gmail.com>
2013-08-22 15:23:03 -0600

    Wrapped send mail to catch SMTPRecipeintsRefused error.
    Falls back on settings.DEFAULT_FROM_EMAIL.

--- django_contact_form/views.py
+++ django_contact_form/views.py
@@ -1,3 +1,5 @@
+from smtplib import SMTPRecipientsRefused
+
 from django.conf import settings
 from django.contrib.sites.models import Site
 from django.core.mail import send_mail
@@ -5,9 +7,18 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+
 from django_contact_form.forms import ContactForm
 
 
+def send_mail_wrapped(cleaned_data, current_site_name, recipients, sender_email):
+    send_mail(
+        'Message from %s' % current_site_name,  # Email Subject
+        cleaned_data['message'],  # Email Body
+        sender_email,  # Email From Value (Sender)
+        [i[1] for i in recipients],  # Email Recipients
+    )
+    
 def contact(request):
     try:
         current_site_name = Site.objects.get_current().name
@@ -18,13 +29,14 @@ def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            cd = form.cleaned_data
-            send_mail(
-                'Message from %s' % current_site_name,  # Email Subject
-                cd['message'],  # Email Body
-                cd.get('email', cd['email']),  # Email From Value (Sender)
-                [i[1] for i in recipients],  # Email Recipients
-            )
+            cleaned_data = form.cleaned_data
+            sender_email = cleaned_data.get('email', cleaned_data['email'])
+
+            try:
+                send_mail_wrapped(cleaned_data, current_site_name, recipients, sender_email)
+            except SMTPRecipientsRefused:  # Some clients (Google, Microsoft) want you to use their SMTP servers
+                send_mail_wrapped(cleaned_data, current_site_name, recipients, settings.DEFAULT_FROM_EMAIL)
+
             return HttpResponseRedirect(reverse('success'))
     else:
         form = ContactForm()
